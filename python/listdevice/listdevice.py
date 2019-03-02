c6d4e5aed030c2f98b3c83fdb4dc2f980049833f
hanhiver <13910395157@139.com>
2019-03-03 00:07:06 +0800

    update format tuning for listdevice.

--- python/listdevice/listdevice.py
+++ python/listdevice/listdevice.py
@@ -15,27 +15,27 @@ def main():
 		print('Interface #{}: '.format(index))
 		index += 1
 
-		print('    name: {}'.format(item.name.decode()))
-		print('    id:   {}'.format(item.id.decode()))
-		print('    type: {}'.format(item.type))
+		print('    name : {}'.format(item.name.decode()))
+		print('    id   : {}'.format(item.id.decode()))
+		print('    type : {}'.format(item.type))
 
 		if item.type == TY_INTERFACE_TYPE_LIST['TY_INTERFACE_ETHERNET'] \
 			or item.type == TY_INTERFACE_TYPE_LIST['TY_INTERFACE_ETHERNET']:
 
-			print('     - MAC:       {}'.format(item.netInfo.mac.decode()))
-			print('     - ip:        {}'.format(item.netInfo.ip.decode()))
-			print('     - netmask:   {}'.format(item.netInfo.netmask.decode()))
-			print('     - gateway:   {}'.format(item.netInfo.gateway.decode()))
-			print('     - broadcast: {}'.format(item.netInfo.broadcast.decode()))
+			print('     - MAC       : {}'.format(item.netInfo.mac.decode()))
+			print('     - ip        : {}'.format(item.netInfo.ip.decode()))
+			print('     - netmask   : {}'.format(item.netInfo.netmask.decode()))
+			print('     - gateway   : {}'.format(item.netInfo.gateway.decode()))
+			print('     - broadcast : {}'.format(item.netInfo.broadcast.decode()))
 
 		devs = TY_getDeviceList(tylib, item.id)
 		print('     {} devices found in the interface. '.format(len(devs)))	 
 
 		for dev in devs:
+			print("     - device {}: ".format(dev.id.decode()));
 			if dev.iface.type  == TY_INTERFACE_TYPE_LIST['TY_INTERFACE_ETHERNET'] \
 				or dev.iface.type == TY_INTERFACE_TYPE_LIST['TY_INTERFACE_ETHERNET']:
-
-				print("      - device {}: ".format(dev.id));
+				
 				print("            vender     : ".format(dev.vendorName.decode()))
 				print("            model      : ".format(dev.modelName.decode()))
 				print("            device MAC : ".format(dev.netInfo.mac.decode()))
@@ -43,8 +43,8 @@ def main():
 
 			else:
 
-				print("     - Dev vonder {}: ".format(dev.vendorName.decode()))
-				print("     - Dev model  {}: ".format(dev.modelName.decode()))
+				print("            vender : {}: ".format(dev.vendorName.decode()))
+				print("            model  : {}: ".format(dev.modelName.decode()))
 
 	tylib.TYDeinitLib()
 	print("Done!")
